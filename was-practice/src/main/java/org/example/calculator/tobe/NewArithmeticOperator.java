69c0396a72f5bb9fff4f85e9d243bc516f0412f8
Song991123 <109492769+Song991123@users.noreply.github.com>
2023-02-08 22:37:35 +0900

    02.08

--- was-practice/src/main/java/org/example/calculator/tobe/NewArithmeticOperator.java
+++
@@ -1,9 +0,0 @@
-package org.example.calculator.tobe;
-
-import org.example.calculator.domain.PositiveNumber;
-
-/**계산할 데이터를 담을 인터페이스*/
-public interface NewArithmeticOperator {
-    boolean supports(String operator);
-    int calcurate(PositiveNumber operand1, PositiveNumber operand2);
-}
